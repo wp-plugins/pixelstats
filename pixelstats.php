2c36959a23cc833c7d05fc0b812bae1de101c8bc
pixelstats <pixelstats@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-03-04 07:22:56 +0000

    version number in source fixed
    git-svn-id: https://plugins.svn.wordpress.org/pixelstats/trunk@99435 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- pixelstats.php
+++ pixelstats.php
@@ -4,7 +4,7 @@ Plugin Name: pixelstats
 Plugin URI: http://www.arrogant.de/pixelstats/
 Description: Generates statistics about article views for each post using counter pixel.
 Author: Timo Fuchs <pixelstats@arrogant.de>
-Version: 0.7.2 devel
+Version: 0.7.2
 Author URI: http://www.arrogant.de
 License: GPLv3
 */
